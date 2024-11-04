d1b2b9a62273c5e10aa14a7839dff4e29dcc2a35
michaelprooney <rooney13@purdue.edu>
2024-11-04 09:36:26 -0500

    Build for radix tree

---
+++ autogen_radix_tree.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Running aclocal..."
+aclocal
+
+echo "Running autoupdate..."
+autoupdate
+
+echo "Running autoconf..."
+autoconf
+
+echo "Running automake with --add-missing..."
+automake --add-missing
+
+echo "Creating build directory..."
+mkdir -p build
+cd build
+
+echo "Configuring the build..."
+../configure
+
+echo "Building the project..."
+make
+
+echo "Build complete!"
