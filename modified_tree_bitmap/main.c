f60145057e48df10428f928dda0e8e7df326c0a2
Vasileios Georgiou <vgeorgio@purdue.edu>
2024-11-20 17:17:12 -0500

    Correct error messages.

--- modified_tree_bitmap/main.c
+++ modified_tree_bitmap/main.c
@@ -720,14 +720,14 @@ int main(int argc, char **argv)
 {
     //fast_table_init();
     if (argc < 3) {
+        printf("We need specific files for these tests being run.\n");
+        printf("Take a look at how IPv4 (Line 215) and IPv6 (Line 166) addresses are verified.\n");
         printf("Usage: %s <v4_filename> <v6_filename>\n", argv[0]);
-        // exit(-1);
+        exit(-1);
     }
-    printf("We need specific files for these tests being run.\n");
-    printf("Take a look at how IPv4 (Line 215) and IPv6 (Line 166) addresses are verified.\n");
-    // char * v4_filename = argv[1];
-    // char * v6_filename = argv[2];
-    // //ipv4_test(v4_filename);
+    char * v4_filename = argv[1];
+    char * v6_filename = argv[2];
+    ipv4_test(v4_filename);
     // ipv6_test(v6_filename);
     //test_one_prefix();
 
