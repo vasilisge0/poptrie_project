8ded4d910f58c2ec6060c04143b588f1daf6a7b3
Vasileios Georgiou <vgeorgio@purdue.edu>
2024-11-22 15:50:38 -0500

    Adds new test.

--- run_poptrie_tests.sh
+++ run_poptrie_tests.sh
@@ -1,3 +1,4 @@
 cd modified_poptrie/build/;
-./poptrie_test_basic ../tests/linx-rib.20141217.0000-p46.txt;
+./poptrie_test_basic ../tests/linx-rib.20141217.0000-p46.txt
+# ./poptrie_test_basic ../tests/ipv4.rib20141217.0000.txt;
 cd ../../;
